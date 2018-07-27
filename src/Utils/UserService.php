732db3c77a87ac184890508676e6f7af4e29aa58
avezen <todiexxx@gmail.com>
2018-07-27 14:05:33 +0200

    bug fixes

--- src/Utils/UserService.php
+++ src/Utils/UserService.php
@@ -23,28 +23,27 @@ class UserService{
 
         $doUserExist = $this->em->getRepository(User::class)->findBy(array("login"=>$registerData->getLogin()));
         if($doUserExist !== []){
-            return "Konto o podanym loginie już istnieje";
+            return "Account with this login already exists";
         }
-        if(strlen($registerData->getLogin()) >= 5 || strlen($registerData->getPassword()) >= 5) {
+        if(strlen($registerData->getLogin()) > 5 || strlen($registerData->getPassword()) > 5) {
             if ($registerData->getPassword() === $registerData->getPasswordRepeated()) {
                 $user = new User();
-                $roles = array("0" => "ROLE_USER");
+                $roles = array("0"=>"ROLE_USER");
                 $user->setLogin($registerData->getLogin());
                 $user->setPassword(password_hash($registerData->getPassword(), PASSWORD_BCRYPT, array('cost' => 11)));
                 $user->setUsername($registerData->getUsername());
                 $user->setRoles(serialize($roles));
                 $user->setRegistrationDate($now);
                 $user->setIsActive(0);
-
                 $this->em->persist($user);
                 $this->em->flush();
 
                 return true;
             }
 
-            return "Podane hasła nie są identyczne";
+            return "Passwords are not the same";
         }
-        return "Podane hasło lub login jest za krótkie. Muszą mieć więcej niż 5 znaków";
+        return "Password or login are too short. They need to be longer than 5";
     }
 
     public function loginUser($loginData){
@@ -57,15 +56,13 @@ class UserService{
             ->getArrayResult();
 
         if($loggedUser !== []){
-            if(password_verify($loginData->getPassword(), $loggedUser[0]['password']))
-            if($loggedUser[0]['isActive'] === 0)
-                return 0;
+            if(password_verify($loginData->getPassword(), $loggedUser[0]['password'])) {
+                if ($loggedUser[0]['isActive'] === 0)
+                    return 0;
 
-            return $loggedUser;
+                return $loggedUser;
+            }
         }
-
-
-
         return false;
     }
 }
